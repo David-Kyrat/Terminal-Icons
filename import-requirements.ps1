30cc878dcf00acf68074bf087c30b1d556fe02cb
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-14 21:23:49 +0200

    clean

--- import-requirements.ps1
+++
@@ -1,7 +0,0 @@
-# NB: Will not work if they haven't been installed with `Install-Module ...` (see version in requirements.ps1)
-
-Import-Module BuildHelpers     
-Import-Module PowerHtml        
-Import-Module PowerShellBuild  
-Import-Module psake            
-Import-Module PSScriptAnalyzer 
